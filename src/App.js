55c9ac2abed7dc327a9c2962e94df6f60269aa09
karanyogi <karankota01@example.com>
2020-09-09 16:10:17 +0530

    till assignment 1 + week2 3 Lectures

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React,{Component} from 'react';
-import {Navbar, NavbarBrand} from 'reactstrap';
+import Main from './components/MainComponents';
 import './App.css';
 
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorant Con fusion</NavbarBrand>
-          </div>
-        </Navbar>
+      <div >
+        <Main />
       </div>
     )
   }
---
+++ src/components/DishDetails.js
@@ -0,0 +1,78 @@
+import React,{ Component } from "react";
+import { Media, Card, CardImg, CardImgOverlay, CardText,CardBody,CardTitle, CardHeader } from 'reactstrap';
+
+class DishDetails extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    renderDish(dish) {
+        if (dish != null)
+            return(
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                      <CardTitle>{dish.name}</CardTitle>
+                      <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        else
+            return(
+                <div></div>
+            );
+    }
+
+
+
+    dishDetails(dish) {
+        if (dish != null)
+        {
+            const menu = dish.comments.map((comment1) => {
+                return (
+                  <div >
+                    <ul className="list-unstyled">
+                    <li>{comment1.comment}</li>
+                    </ul>
+                    <ul className="list-unstyled">
+                    <li>--{comment1.author}, {new Intl.DateTimeFormat('en-US',{ year: 'numeric', month: 'short', day:'2-digit' }).format(new Date(Date.parse(comment1.date)))} </li>
+                    </ul>
+                    </div>
+                  );
+                });
+                return (
+                    <div className="container">
+                        <h4>Comments</h4>
+                        {menu}
+                    </div>
+                );
+        }
+        else
+            return(
+                <div></div>
+            );
+    }
+
+
+
+    render(){
+        return(
+            <div className="container">
+            <div className="row">
+                <div  className="col-12 col-md-5 m-1">
+                  {this.renderDish(this.props.dish)}
+                </div>
+                <div className="col-12 col-md-5 m1">
+                
+                    {this.dishDetails(this.props.dish)}
+                </div>
+            </div>
+            </div>
+      );
+    }
+
+
+}
+
+export default DishDetails;
